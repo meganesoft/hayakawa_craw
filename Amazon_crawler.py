d21dffb2dcfb37b5a1ad8a2ba5f733b65a2af142
megene <meganesoft87@gmail.com>
2018-07-08 01:54:58 +0900

    csv 書き込みできた

--- Amazon_crawler.py
+++ Amazon_crawler.py
@@ -1,21 +1,20 @@
 import requests # urlを読み込むためrequestsをインポート
 from bs4 import BeautifulSoup # htmlを読み込むためBeautifulSoupをインポート
 import pandas as pd
-
-def write_csv(*args,**kwargs):
-    df = pd.read_csv('book.csv')
+import os.path
+#def write_csv(func):
+   # df = pd.read_csv('book.csv')
     #画像のタイトルや概要テキストをCSVに保存する処理を書く
-    def decorator(f):
+    #def decorator(f):
         
         
 
-def write_image(URL):
+def write_data(URL):
     images = [] # 画像リストの配列
-
+    csv_data = pd.read_csv("data/book.csv")
     soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
-    print("kokomade")
+    
     #print(soup.find(id="M_itemImg").findAll("img"))
-    print(soup.find(id="M_itemDetail").findAll("p"))
     for link in soup.find(id="M_itemImg").findAll("img"): # imgタグを取得しlinkに格納
         if link.get("src").endswith(".jpg"): # imgタグ内の.jpgであるsrcタグを取得
             images.append(link.get("src")) # imagesリストに格納
@@ -26,8 +25,23 @@ def write_image(URL):
         re = requests.get(target)
         with open('img/' + target.split('/')[-1], 'wb') as f: # imgフォルダに格納
             f.write(re.content) # .contentにて画像データとして書き込む
- 
+    
+    for link in soup.find(id="M_itemDetail").findAll("p"):
+        print("kokomade")
+        print(link.getText())
+        csv_data["text"] = str(link.getText())
+
+    print(csv_data["text"])
+    csv_data.to_csv("data/book.csv",encoding="shift_jis")
     print("ok") # 確認
+
+#dataディレクトリにcsvファイルが作成されていない時にファイルを作成する
+def create_csv():
+    if os.path.isfile("data/book.csv"):
+        pass
+    else:
+        csv_data = pd.DataFrame([["1","1","1"]],columns=["url","image","text"])
+        csv_data.to_csv("data/book.csv")
 
 def write_text(URL):
     soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
@@ -47,7 +61,8 @@ def crawl_web(seed):
 
 def main():
     url = "http://www.hayakawa-online.co.jp/shopdetail/000000013936/genre_001002/page1/order/"
-    write_image(url)
+    create_csv()
+    write_data(url)
 
 if __name__ == '__main__':
     main()
